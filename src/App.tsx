c46f14f4cae5fa82de16c613f4497df81f48ffba
Kendrick Elifritz <kendrickelifritz@gmail.com>
2024-05-28 09:57:52 -0700

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  )
+}
+
+export default App
